90ab4f8fd18f8b274cae7c1819e6c5626d79df17
marufiqbal5g <kmiqbal1989@gmail.com>
2026-04-08 12:59:09 +0600

    Fixed Password Reset and Registration

--- users/models.py
+++ users/models.py
@@ -11,12 +11,23 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    # def save(self):
+    #     super().save()
 
-        img = Image.open(self.image.path)
+    #     img = Image.open(self.image.path)
+
+    #     if img.height > 300 or img.width > 300:
+    #         outpur_size = (300,300)
+    #         img.thumbnail(outpur_size)
+    #         img.save(self.image.path)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.image and hasattr(self.image, 'path'):
+            img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
-            outpur_size = (300,300)
-            img.thumbnail(outpur_size)
+            output_size = (300, 300)
+            img.thumbnail(output_size)
             img.save(self.image.path)
