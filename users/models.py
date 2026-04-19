90036a5a13b62b29d379078d8feb76db7f49beb7
marufiqbal5g <kmiqbal1989@gmail.com>
2026-04-19 18:26:21 +0600

    Random Updated

--- users/models.py
+++ users/models.py
@@ -11,15 +11,6 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    # def save(self):
-    #     super().save()
-
-    #     img = Image.open(self.image.path)
-
-    #     if img.height > 300 or img.width > 300:
-    #         outpur_size = (300,300)
-    #         img.thumbnail(outpur_size)
-    #         img.save(self.image.path)
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
